2b7f1fc0a76189d2aa223d33e1b6f99b6db2ebbd
Dev-Nonso <chinonsoogbonnaya47@gmail.com>
2023-11-28 15:24:48 -0800

    printing double number in looping

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for k in range(i + 1, 10):
         if i == 8 and k == 9:
-            print("{}{}".format(i,k))
+            print("{}{}".format(i, k))
         else:
             print("{}{}, ".format(i, k), end="")
