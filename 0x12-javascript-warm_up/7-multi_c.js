5e27550c4d4c866ec9f3d2138c7614e710e067ea
Dev-Nonso <chinonsoogbonnaya47@gmail.com>
2024-03-11 15:44:15 -0700

    updated

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,8 @@ if (process.argv[2] === undefined || isNaN(process.argv[2])) {
 } else {
   const x = Number(process.argv[2]);
   let i = 0;
-  for (; i < x; i++){
-	  console.log('C is fun');
+  while (i < x) {
+    console.log('C is fun');
+    i++;
   }
 }
